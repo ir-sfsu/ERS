1a229ecfd5dac98b5ca819f55977b6c5c010813d
David Ranzolin <daranzolin@gmail.com>
2021-10-15 11:35:26 -0700

    update

--- R/clean_raw_ers_txt_file.R
+++ R/clean_raw_ers_txt_file.R
@@ -6,14 +6,15 @@
 #'
 #' @return a data frame
 #' @export
-clean_raw_ers_txt_file <- function(file, type = c("ERSA", "ERSS", "ERSD", "ERSFA"), ...) {
+clean_raw_ers_txt_file <- function(file, type = c("ERSA", "ERSS", "ERSD", "ERSFA", "ERSR"), ...) {
 
   type <- match.arg(type)
   layout <- switch(type,
                    "ERSA"  = ERS::ersa,
                    "ERSS"  = ERS::erss,
                    "ERSD"  = ERS::ersd,
-                   "ERSFA" = ERS::ersfa)
+                   "ERSFA" = ERS::ersfa,
+                   "ERSR"  = ERS::ersr)
 
   out <- readr::read_fwf(file, readr::fwf_widths(layout[["Length"]]))
   names(out) <- layout[["Field Name"]]
